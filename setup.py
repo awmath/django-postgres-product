303b34df749e9b8242cbef6868ec3b0ed05639c2
Axel Wegener <awmath@sparse-space.de>
2020-09-17 14:58:42 +0200

    push version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages('.', exclude=['tests']),
     # Start with a small number and increase it with
     # every change you make https://semver.org
-    version='1.0',
+    version='1.0.1',
     # Chose a license from here: https: //
     # help.github.com / articles / licensing - a -
     # repository. For example: MIT
