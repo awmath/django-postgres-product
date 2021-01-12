fc50a8213d03016684accc45b73b86dfa75e849d
awmath <awmath@sparse-space.de>
2021-01-12 09:50:13 +0000

    Initial gitpod settings

--- tests/settings.py
+++ tests/settings.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': 'test',
         'USER': 'test',
         'PASSWORD': 'test',
-        'HOST': 'postgres',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
