b13d6ca4e5885354e97f4ee1fcda74aecd0c9a4c
awmath <awmath@sparse-space.de>
2021-12-09 09:33:51 +0000

    chore: matrix testing with pytest

--- run-postgres.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -it -p 5432:5432 -e POSTGRES_PASSWORD=test -e POSTGRES_USER=test --name postgres postgres
--- tests/settings.py
+++ tests/settings.py
@@ -1,13 +1,18 @@
-SECRET_KEY = 'fake-key'
-INSTALLED_APPS = ['tests', 'postgres_product']
+# -*- coding: utf-8 -*-
+SECRET_KEY = "fake-key"
+INSTALLED_APPS = ["tests", "postgres_product"]
+
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'postgres',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": "test",
+        "PASSWORD": "test",
+        "HOST": os.environ.get("DB_HOST", "postgres"),
+        "PORT": "5432",
     }
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
